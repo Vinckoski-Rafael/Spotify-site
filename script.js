958588c43755a9c50ff201564a3047f4cf0331bc
Rafael Vinckoski Roque <rafael_v_roque@hotmail.com>
2025-02-04 22:19:52 -0300

    Implementação da API Fake, e geração da main do proejto.

---
+++ script.js
@@ -0,0 +1,39 @@
+const searchInput=document.getElementById('search-input');
+const resultArtists=document.getElementById('result-artist');
+const resultPlaylists=document.getElementById('result-playlists');
+
+function requestApi(searchTerm){
+    const url= `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+    .then((response)=>response.json())
+    .then((result)=>displayResults(result))
+}
+
+
+function displayResults(result){
+    resultPlaylists.classList.add('hidden');
+    const artistName=document.getElementById('artist-name');
+    const artistImage=document.getElementById('artist-img');
+
+    result.forEach(element => {
+       artistName.innerText=element.name;
+       artistImage.src=element.urlImg; 
+    });
+
+    resultArtists.classList.remove('hidden');
+}
+
+
+document.addEventListener('input', function(){
+    const searchTerm = searchInput.value;
+
+    if(searchTerm===''){
+        resultPlaylists.classList.add('hidden');
+        resultArtists.classList.remove('hidden');
+        return;
+    }
+        
+    console.log(searchTerm);
+    requestApi(searchTerm);
+
+})
